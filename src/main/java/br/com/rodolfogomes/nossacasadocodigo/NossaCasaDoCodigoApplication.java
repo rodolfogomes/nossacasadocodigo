e681163fe9d48ee2eaf17a41899834b7835d7178
rodolfo <rodolfoaguiargomes@gmail.com>
2020-10-12 14:28:47 -0300

    Projeto configurado. Hora do show

---
+++ src/main/java/br/com/rodolfogomes/nossacasadocodigo/NossaCasaDoCodigoApplication.java
@@ -0,0 +1,13 @@
+package br.com.rodolfogomes.nossacasadocodigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NossaCasaDoCodigoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NossaCasaDoCodigoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/rodolfogomes/nossacasadocodigo/NossaCasaDoCodigoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rodolfogomes.nossacasadocodigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NossaCasaDoCodigoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
